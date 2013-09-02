bf6345c48a2dc7014ad526ce2bd479fe147cbd57
Aaron VonderHaar <gruen0aermel@gmail.com>
2013-09-01 22:27:12 -0700

    Move datum API to https://github.com/avh4/perdata-datum

--- src/test/java/net/avh4/data/datum/MemoryDatumStoreTest.java
+++
@@ -1,7 +0,0 @@
-package net.avh4.data.datum;
-
-public class MemoryDatumStoreTest extends DatumStoreContract {
-    @Override protected DatumStore createSubject() {
-        return new MemoryDatumStore();
-    }
-}
